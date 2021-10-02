dd2ecad66664a6fcec9919ac2096a525f2a0f8b7
v <villekum@metropolia.fi>
2021-10-02 17:57:48 +0300

    Optimized animation to use fewer cycles.

--- public/js/script.js
+++ public/js/script.js
@@ -3,13 +3,14 @@ window.addEventListener('DOMContentLoaded', (event) => {
     //  logo fan handling
     const fan =  document.getElementById("fan")
     let currentRot = 0
+    fan.style.transition = "all 0.25s";
 
     function showTime() {
     fan.style.transform= "rotate(" + currentRot + "deg)"
-        currentRot++
+        currentRot+=20
     }
 
-    setInterval(showTime, 1);
+    setInterval(showTime, 100);
     //  logo fan handling
 
 });
