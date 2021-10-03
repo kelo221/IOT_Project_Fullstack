81bea27dede786caf0354b98340c8de764502cf1
v <villekum@metropolia.fi>
2021-10-03 16:34:30 +0300

    Added page functionalities: Button handling, Containers for data, Custom SASS (WIP)

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,11 @@
+"use strict";
+
 window.addEventListener('DOMContentLoaded', (event) => {
+    let homeDiv = document.getElementById("homeContent");
+    let graphDiv = document.getElementById("graphContent");
+    const homeButton =  document.getElementById("homeButton")
+    const graphButton =  document.getElementById("graphButton")
+    graphDiv.style.display = "none";
 
     //  logo fan handling
     const fan =  document.getElementById("fan")
@@ -12,6 +19,28 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     setInterval(showTime, 100);
     //  logo fan handling
+
+
+    //  home button handling
+    homeButton.addEventListener("click", () => {
+        console.log("homeButton clicked.");
+        if (homeDiv.style.display === "none") {
+            homeDiv.style.display = "block";
+            graphDiv.style.display = "none";
+        }
+    });
+    //  home button handling
+
+
+    //  graph button handling
+    graphButton.addEventListener("click", () => {
+        console.log("graphButton clicked.");
+        if (graphDiv.style.display === "none") {
+            homeDiv.style.display = "none";
+            graphDiv.style.display = "block";
+        }
+    });
+    //  graph button handling
 
 });
 
