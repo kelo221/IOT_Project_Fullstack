bc281292b74ff9ec56a2a62ad69c08a19416e13e
v <villekum@metropolia.fi>
2021-10-04 16:33:48 +0300

    moving pointer, broken atm

--- public/js/script.js
+++ public/js/script.js
@@ -2,36 +2,38 @@
 
 let systemIsAutomatic = true
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    const homeDiv = document.getElementById("homeContent");
-    const graphDiv = document.getElementById("graphContent");
-    graphDiv.style.display = "none";
+window.onload = function() {
+    const homeDiv = document.getElementById("homeContent")
+    const graphDiv = document.getElementById("graphContent")
+    graphDiv.style.display = "none"
     const homeButton = document.getElementById("homeButton")
     const graphButton = document.getElementById("graphButton")
-    const gaugeSvg = document.getElementById('fanGauge').contentDocument;
-    const pointer = gaugeSvg.getElementById('pointer');
-    const modeSwitch = document.getElementById("switchImage");
+    const gaugeSvg = document.getElementById('fanGauge').contentDocument
+    const pointer = gaugeSvg.getElementById('pointer')
+    const modeSwitch = document.getElementById("switchImage")
 
     //  Logo fan handling
     const fan = document.getElementById("fan")
     let currentRot = 0
-    fan.style.transition = "all 0.25s";
+    fan.style.transition = "all 0.25s"
 
     function showTime() {
         fan.style.transform = "rotate(" + currentRot + "deg)"
+        pointer.style.transform =  "rotate(" + currentRot + "deg)"
+       // pointer.style.transform = "translate(-87px, -67px)"
         currentRot += 20
     }
 
-    setInterval(showTime, 100);
+    setInterval(showTime, 100)
     //  Logo fan handling
 
 
     //  Home button handling
     homeButton.addEventListener("click", () => {
-        console.log("homeButton clicked.");
+        console.log("homeButton clicked.")
         if (homeDiv.style.display === "none") {
-            homeDiv.style.display = "block";
-            graphDiv.style.display = "none";
+            homeDiv.style.display = "block"
+            graphDiv.style.display = "none"
         }
     });
     //  Home button handling
@@ -39,26 +41,35 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     //  Graph button handling
     graphButton.addEventListener("click", () => {
-        console.log("graphButton clicked.");
+        console.log("graphButton clicked.")
         if (graphDiv.style.display === "none") {
-            homeDiv.style.display = "none";
-            graphDiv.style.display = "block";
+            homeDiv.style.display = "none"
+            graphDiv.style.display = "block"
         }
     });
     //  Graph button handling
 
-    /*
-        // pointer
-        pointer.setAttribute('transform-origin', '0 20');
-        pointer.setAttribute("transform", "rotate(100)");
-        // pointer
-    */
 
-    console.log(gaugeSvg)
+        // pointer
+/*        pointer.setAttribute('transform-origin', '0 20');
+        pointer.setAttribute("transform", "rotate(100)");*/
+
+
+
+        // pointer
+    // pointer.style.transformOrigin = '65% 58%';
+
+    pointer.style.transition = "all 0.25s"
+//    pointer.style.transformOrigin = '70% 80%'
+    pointer.style.transformOrigin = '310px 275px'
+
+    //pointer.style.transform =  "rotate(10deg)"
+    //pointer.style.transform = "translate(-87px, -67px)"
+    console.log(pointer)
 
     // Mode switch button
     modeSwitch.addEventListener("click", () => {
-        console.log("switchImage clicked.");
+        console.log("switchImage clicked.")
         if (systemIsAutomatic) {
             modeSwitch.src = "img/switchM.png"
             systemIsAutomatic = false
@@ -68,5 +79,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
         }
     });
     // Mode switch button
-});
+};
+
 
